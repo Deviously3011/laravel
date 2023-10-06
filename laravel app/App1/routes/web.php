9c6aa97f0f292b37df677c1ee0b45391dcc39fb7
dejah marshall <2165302@talnet.nl>
2023-10-06 15:54:55 +0200

    opdracht
    nieuwe opdracht

--- laravel app/App1/routes/web.php
+++ laravel app/App1/routes/web.php
@@ -9,35 +9,5 @@ Route::get('/', function () {
 Route::get('/planetss', function () {
     return ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
 });
-
-Route::get('/planets/{planet?}', function ($planetName = null) {
-    $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-
-    // Convert the array to a collection for easier filtering
-    $planetsCollection = collect($planets);
-
-    if ($planetName) {
-        $planetsCollection = $planetsCollection->filter(function ($planet) use ($planetName) {
-            return strtolower($planet['name']) === strtolower($planetName);
-        });
-    }
-
-    return view('planets', ['planets' => $planetsCollection->all()]);
-});
+Route::get('/planets', [App\Http\Controllers\PlanetController::class, 'index']);
+Route::get('/planets/{planet}', [App\Http\Controllers\PlanetController::class, 'show']);
