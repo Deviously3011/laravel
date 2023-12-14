42076df306820e01d6f16fd49d9c9e0c47180f7c
dejah marshall <2165302@talnet.nl>
2023-12-14 11:56:40 +0100

    eloquent relaties

--- laravel app/App1/app/Models/Planet.php
+++ laravel app/App1/app/Models/Planet.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Planet extends Model
 {
-    protected $table = 'planets';
-
-    // Other model configurations and relationships can be added here
-} 
+    public function solarSystem()
+    {
+        return $this->belongsTo(SolarSystem::class);
+    }
+}
