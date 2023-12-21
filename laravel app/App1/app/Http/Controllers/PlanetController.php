f6b52bd2e748bb900062cb8798db830e13047868
dejah marshall <2165302@talnet.nl>
2023-12-21 12:04:32 +0100

    update

--- laravel app/App1/app/Http/Controllers/PlanetController.php
+++ laravel app/App1/app/Http/Controllers/PlanetController.php
@@ -6,15 +6,17 @@ use Illuminate\Http\Request;
 
 class PlanetController extends Controller
 {
+ 
     public function index()
-{
-    $planets = Planet::all();
-    return view('planets.index', ['planets' => $planets]);
-}
-
-public function sahow($planetName)
-{
-    $planet = Planet::where('name', $planetName)->first();
-    return view('planets.show', ['planet' => $planet]);
-}
+    {
+        $planets = Planet::all();
+        return view('planets.index', ['planets' => $planets]);
+    }
+    
+    public function show($planetName)
+    {
+        $planet = Planet::where('name', $planetName)->first();
+        return view('planets.show', ['planet' => $planet]);
+    }
+    
 }
